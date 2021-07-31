86cbc90d34999ad20d4141d6f4242957125e78e9
Alexey Rudoy <ao.rudoy@yandex.ru>
2021-08-01 01:04:32 +0300

    feat: добавить компонент PizzaBlock

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import { Header } from './components';
 import { Home, Cart } from './pages/';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    return fetch('http://localhost:3000/db.json')
+      .then((res) => res.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
   return (
     <>
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route exact path ='/' component={Home} />
-          <Route exact path ='/cart' component={Cart} />
+          <Route exact path="/">
+            <Home items={pizzas} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
         </div>
       </div>
     </>
